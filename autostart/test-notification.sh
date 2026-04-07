0cf8330c87f5533db8a3d2113efd63dba4448326
Yaşar Kumru <yasar.kumru@adalet.gov.tr>
2026-04-07 13:38:59 +0300

    another test

--- autostart/test-notification.sh
+++ autostart/test-notification.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-notify-send "Autostart Test" "this is another change to test if it is working!"
+notify-send "Autostart Test" "this is second change to test if it is working!"
