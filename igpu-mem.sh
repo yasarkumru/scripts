2296dad366d145bff27bcfbc3e91b7fc7832bf09
Yaşar Kumru <yasar.kumru@adalet.gov.tr>
2026-04-21 09:23:54 +0300

    igpu mem generic write

--- igpu-mem.sh
+++ igpu-mem.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-bytes=$(sudo awk 'NR==1 { print $(NF-1) }' /sys/kernel/debug/dri/1/i915_gem_objects 2>/dev/null)
+gem_file=$(sudo find /sys/kernel/debug/dri/ -name i915_gem_objects 2>/dev/null | head -1)
+bytes=$(sudo awk 'NR==1 { print $(NF-1) }' "$gem_file" 2>/dev/null)
 gb=$(echo "scale=2; $bytes / 1024 / 1024 / 1024" | bc)
 echo "iGPU: ${gb} GB"
